5c843f41a19a075cb36e64b59f1f125fd42ca6db
Daniel Fuentes <141572206+upb-danielfuentes@users.noreply.github.com>
2024-02-08 20:52:53 -0500

    2.0

--- app/src/main/java/com/danielfuentes/chocowallet/SplashScreenActivity.kt
+++ app/src/main/java/com/danielfuentes/chocowallet/SplashScreenActivity.kt
@@ -1,11 +1,24 @@
 package com.danielfuentes.chocowallet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        nextView()
+    }
+    private fun nextView() {
+        object: CountDownTimer(3000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {}
+            override fun onFinish() {
+                val intent = Intent(applicationContext, AboutActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }.start()
     }
 }
